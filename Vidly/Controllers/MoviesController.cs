5d452c18235112dd5f863784724c4e743d3d08fd
unknown <gremcleo@gmail.com>
2020-03-11 20:10:38 -0400

    initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie(){Name = "Shrek!"};
+
+            //return View(movie);
+            return RedirectToAction("Index", "Home", new {page=1, sortBy="name"});
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            return Content("Id"+ Id);
+
+        }
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (sortBy.IsNullOrWhiteSpace())
+                sortBy = "Name";
+            return Content(String.Format("pageIndex = {0}, sortBy = {1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+
+        }
+    }
+}
