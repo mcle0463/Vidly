db8b8df11bbce1d42db818bdcf73f3a3b5653177
unknown <gremcleo@gmail.com>
2020-03-30 21:38:07 -0400

    Update identityConfig and minor schema change to Customers table

--- Vidly/App_Start/IdentityConfig.cs
+++ Vidly/App_Start/IdentityConfig.cs
@@ -10,10 +10,30 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
 using Vidly.Models;
 
+//[assembly: OwinStartup(typeof(Vidly.App_Start.Startup1))]
 namespace Vidly
 {
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(() => new ApplicationDbContext());
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            /*app.CreatePerOwinContext<RoleManager<AppRole>>((options, context) =>
+                new RoleManager<AppRole>(
+                    new RoleStore<AppRole>(context.Get<MyDbContext>())));*/
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Home/Login"),
+            });
+        }
+    }
     public class EmailService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
