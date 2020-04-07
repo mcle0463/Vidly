1808207eeb9a86253d814908a1fd224df562bd61
unknown <gremcleo@gmail.com>
2020-04-06 22:02:16 -0400

    migrations for MembershipType

---
+++ Vidly/Migrations/202004070048343_AddNameValuesToMemebershipTypesTable.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameValuesToMemebershipTypesTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET MembershipTypes.[Name] = 'Pay as you go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET MembershipTypes.[Name] = 'Monthly' WHERE Id = 2");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
