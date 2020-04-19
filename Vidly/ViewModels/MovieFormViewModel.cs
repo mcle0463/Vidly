c22734a05ad60085655f2b78c6f5b1ded6405360
Greg Mcleod <G-mcleod@hotmail.com>
2020-04-19 15:51:36 -0400

    Update Movie to user ViewModel and add associated migration

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -7,6 +7,16 @@ namespace Vidly.ViewModels
     {
         public IEnumerable<Genre> GenreTypes { get; set; }
         public Movie Movie { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Movie != null && Movie.Id != 0)
+                    return "Edit Movie";
+
+                return "New Movie";
+            }
+        }
 
     }
 }
